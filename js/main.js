a4bf0726477b8ef8b3bdc6dd6e9d532d6c048fcc
Weilin Chen <weilin_chen@xinguard.com>
2014-11-18 20:40:57 +0800

    Add RequireJS support.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+require(['../bower_components/jquery/dist/jquery.min', 
+        '../bower_components/jsviews/jsrender.min',
+        '../bower_components/bootstrap/dist/js/bootstrap.min',
+        'app'], function () {
+            console.log($);
+        })
